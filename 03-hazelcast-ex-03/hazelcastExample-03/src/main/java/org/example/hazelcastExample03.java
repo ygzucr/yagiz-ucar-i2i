576a01a365283d1130723672f536519452b553a7
ygzucr <111087713+ygzucr@users.noreply.github.com>
2025-07-03 23:45:02 +0300

    03-hazelcast-ex-03

---
+++ 03-hazelcast-ex-03/hazelcastExample-03/src/main/java/org/example/hazelcastExample03.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+public class hazelcastExample03 {
+    public static void main(String[] args) {
+
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().addAddress("13.51.207.134:5701");
+
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+        IMap<Integer, Person> map = client.getMap("personMap");
+
+        for (int personNumber=0; personNumber<10; personNumber++){
+           map.put(personNumber, new Person("Person-"+personNumber));
+        }
+
+        System.out.println("10 Person added.");
+
+        for (int getPerson=0; getPerson < 10; getPerson++){
+            System.out.println(map.get(getPerson));
+        }
+
+        System.out.println("Total number of person: "+ map.size());
+
+        client.shutdown();
+
+    }
+
+}
